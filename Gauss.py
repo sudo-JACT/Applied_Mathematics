138d695937572a5fd49d26c7fbfc44472a7409c5
1MD0NK3Y <jacopino.toma@gmail.com>
2025-12-02 20:37:35 +0100

    First commit

---
+++ Gauss.py
@@ -0,0 +1,196 @@
+import numpy as np 
+from sympy import *
+from sympy.core.function import UndefinedFunction
+from scipy import *
+
+
+
+def Golub_Welsch(nodes: int) -> list[list[float]]:
+    
+    
+    return [[0.0,0.0,0.0,0.0], [0.0,0.0,0.0,0.0,0.0]]
+
+
+
+
+def Finde_Xk_and_Wk(nodes: int) -> list[list[float]]:
+    
+    
+    m = []
+    
+    
+    try:
+        
+        with open("computed_nodes.csv", "r") as file:
+            
+            l = file.readlines()
+            
+            l = [i.rstrip().split(",") for i in l]
+    
+    
+            c = 0
+            flag = False
+            
+            for i in l:
+                
+                if int(i[0]) == nodes:
+                    
+                    c += 1
+                    
+                    k = [float(j) for j in i]
+                    
+                    k.pop(0)
+                    
+                    m.append(k)
+                    
+                if c == 2:
+                    
+                    flag = True
+                    
+                    break
+        
+            
+            
+            if not(flag):
+                
+                m.clear()
+                
+                file.close()
+                
+                with open("computed_nodes.csv", "a") as file:
+                    
+                    m = Golub_Welsch(nodes)
+                    
+                    n = "" + str(nodes) + ","
+                    w = "" + str(nodes) + ","
+                    
+                    for i in m[0]:
+                        
+                        n += str(i) + ","
+                    
+                    n = n[:-1]
+                    n += "\n"
+                        
+                        
+                        
+                    for i in m[1]:
+                        
+                        w += str(i) + ","
+                        
+                    w = w[:-1]
+                    w += "\n"
+                    
+                    
+                    file.write(n)
+                    file.write(w)
+                        
+
+
+            
+    except:
+        
+        with open("computed_nodes.csv", "w") as file:
+                    
+            m = Golub_Welsch(nodes)
+            
+            n = "" + str(nodes) + ","
+            w = "" + str(nodes) + ","
+            
+            for i in m[0]:
+                
+                n += str(i) + ","
+            
+            n = n[:-1]
+            n += "\n"
+                
+                
+                
+            for i in m[1]:
+                
+                w += str(i) + ","
+                
+            w = w[:-1]
+            w += "\n"
+            
+            
+            file.write(n)
+            file.write(w)
+    
+    
+        
+    return m
+            
+            
+
+
+
+
+def Gauss_Legendre(nodes: int, f: type[UndefinedFunction], s: Symbol) -> float:
+    
+    m = Finde_Xk_and_Wk(nodes)
+    
+    G = 0
+    
+    n = len(m[0])
+    
+    g = lambdify(s, f, "numpy")
+    
+    
+    for i in range(n):
+        
+        G += (m[0][i] * g(m[1][i]))
+        
+    
+    return G
+
+
+
+def Chebyshev_Polynomials(degree: int) -> type[UndefinedFunction]:
+    
+    x = Symbol("x")
+    
+    
+    if degree == 0:
+        
+        Tn = 1
+    
+    elif degree == 1:
+        
+        Tn = x
+    
+    else:
+        
+        Tn = ((2 * x * Chebyshev_Polynomials(degree-1)) - Chebyshev_Polynomials(degree-2))
+        
+    
+    return Tn
+
+
+
+def Chebyshev_Zeros():
+    
+    pass
+
+
+def Chebyshev_Weight_Functions():
+    
+    pass
+
+
+
+def Gauss_Chebyshev() -> float:
+    
+    pass
+
+
+
+
+if __name__ == "__main__":
+    
+    x = Symbol("x")
+    
+    f = cos(x)
+    
+    n = 1
+    
+    print(Gauss_Legendre(n, f, x))
